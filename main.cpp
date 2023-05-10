a765dec623de6e4dbc66c6ebd7bdfe0500e7e806
Bartosz Laskowski <81393810+Nougat00@users.noreply.github.com>
2023-05-11 01:14:26 +0200

    HasIntersection not working

--- main.cpp
+++ main.cpp
@@ -5,19 +5,33 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+
 class Lander {
 public:
     Lander();
+
     ~Lander();
+
     void handleEvent(SDL_Event &e);
-    void update();
-    void render(SDL_Renderer *renderer);
+
+    void update(SDL_Rect *rect1);
+
+    void land();
+
+    void gravity();
+
+    void render(SDL_Renderer *renderer, SDL_Texture *texture);
+
+    SDL_Rect getDst();
+
 private:
     double x, y;
     double vx, vy;
     double angle;
     double throttle;
     double fuel;
+    SDL_Rect src;
+    SDL_Rect dst;
 };
 
 Lander::Lander() {
@@ -58,7 +72,11 @@ void Lander::handleEvent(SDL_Event &e) {
     }
 }
 
-void Lander::update() {
+SDL_Rect Lander::getDst() {
+    return dst;
+}
+
+void Lander::gravity() {
     double dt = 0.01;
     double gravity = 9.81;
     double mass = 1000;
@@ -78,37 +96,11 @@ void Lander::update() {
     vy += ay * dt;
     x += vx * dt;
     y += vy * dt;
-    std::cout<<y<<std::endl;
-
-//    if(SDL_hasIntersection(rect, rect2)) {
-// land()}else {gravity()}
-// void land() {
-// dstRect.y = getRect()
-// }
-//
-//    if(!SDL_hasIntersection(rect, rect2)) {
-// gravity()}
-// void land() {
-// dstRect.y = getRect()
-// }
-
 }
 
-void Lander::render(SDL_Renderer *renderer) {
-    SDL_Surface* surfaceShip = IMG_Load("assets/ship.png");
-    SDL_Texture* textureShip = SDL_CreateTextureFromSurface(renderer, surfaceShip);
-    SDL_Rect srcBgShip;
-    srcBgShip.h = 480;
-    srcBgShip.w = 640;
-    srcBgShip.x = srcBgShip.y = 0;
-    SDL_Rect dstBgShip = {int(x - 5), int(y - 5), 50, 50};
-    SDL_RenderCopy(renderer, textureShip, &srcBgShip, &dstBgShip);
-}
-
-SDL_Texture* texturer(std::string path, SDL_Renderer *renderer) {
-    SDL_Surface* surface = IMG_Load(path);
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    return texture;
+void Lander::land() {
+    x = x;
+    y = y;
 }
 
 SDL_Rect rect(int h, int w, int x, int y) {
@@ -120,24 +112,37 @@ SDL_Rect rect(int h, int w, int x, int y) {
     return rect1;
 }
 
+void Lander::update(SDL_Rect *rect1) {
+    if (SDL_HasIntersection(rect1, &dst) == SDL_TRUE) {
+        std::cout << "test" << std::endl;
+    } else { gravity(); }
+}
+
+SDL_Texture *texturer(const char *path, SDL_Renderer *renderer) {
+    SDL_Surface *surface = IMG_Load(path);
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    return texture;
+}
+
+
+void Lander::render(SDL_Renderer *renderer, SDL_Texture *texture) {
+    src = rect(480, 640, 0, 0);
+    dst = rect(50, 50, x, y);
+    SDL_RenderCopy(renderer, texture, &src, &dst);
+}
+
 int main(int argc, char *argv[]) {
     SDL_Window *window = SDL_CreateWindow("Lunar Lander", SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,
                                           SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_Surface* surfaceHills = IMG_Load("assets/hills.png");
-    SDL_Texture* textureHills = SDL_CreateTextureFromSurface(renderer, surfaceHills);
-    SDL_Rect srcBgHills;
-    srcBgHills.h = 480;
-    srcBgHills.w = 640;
-    srcBgHills.x = srcBgHills.y = 0;
-    SDL_Rect dstBgHills;
-    dstBgHills.h = 480;
-    dstBgHills.w = 640;
-    dstBgHills.x = dstBgHills.y = 0;
-
-    SDL_Rect srcBg = rect(480, 640, 0, 0);
+    SDL_Rect bg = rect(480, 640, 0, 0);
+    SDL_Rect hillsSrc = rect(10, 640, 0, 0);
+    SDL_Rect hillsDes = rect(10, 640, 0, 470);
+    SDL_Texture *bgTexture = texturer("assets/bg.png", renderer);
+    SDL_Texture *hillsTexture = texturer("assets/floor.png", renderer);
+    SDL_Texture *shipTexture = texturer("assets/ship.png", renderer);
 
     Lander lander;
     bool quit = false;
@@ -150,13 +155,16 @@ int main(int argc, char *argv[]) {
             lander.handleEvent(e);
         }
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texturer("assets/bg.png", renderer), &srcBg, &srcBg);
-        SDL_RenderCopy(renderer, textureHills, &srcBgHills, &srcBgHills);
-        lander.update();
+        SDL_RenderCopy(renderer, bgTexture, &bg, &bg);
+        SDL_RenderCopy(renderer, hillsTexture, &hillsSrc, &hillsDes);
+        lander.update(&hillsSrc);
         SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-        lander.render(renderer);
+        lander.render(renderer, shipTexture);
         SDL_RenderPresent(renderer);
+        SDL_Delay(10);
     }
+    SDL_DestroyTexture(bgTexture);
+    SDL_DestroyTexture(hillsTexture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
